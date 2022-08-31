9123662386b703890d342e57bdcc0a9658e2fbf6
ilyas6773 <mister.tolegenov@yandex.ru>
2022-08-31 17:34:55 +0600

    feat: added a character to the game world that can move, but now can't move the camera

--- tiles/main.lua
+++ tiles/main.lua
@@ -20,6 +20,11 @@ VIRTUAL_HEIGHT = 144
 
 TILE_SIZE = 16
 
+CHARACTER_WIDTH = 16
+CHARACTER_HEIGHT = 20
+
+CHARACTER_MOVE_SPEED = 40
+
 -- camera scroll speed
 CAMERA_SCROLL_SPEED = 40
 
@@ -36,15 +41,23 @@ function love.load()
     tilesheet = love.graphics.newImage('tiles.png')
     quads = GenerateQuads(tilesheet, TILE_SIZE, TILE_SIZE)
 
+    -- texture for the character
+    characterSheet = love.graphics.newImage('character.png')
+    characterQuads = GenerateQuads(characterSheet, CHARACTER_WIDTH, CHARACTER_HEIGHT)
+
+    -- place character in middle of the screen, above the top ground tile
+    characterX = VIRTUAL_WIDTH / 2 - (CHARACTER_WIDTH / 2)
+    characterY = ((7 - 1) * TILE_SIZE) - CHARACTER_HEIGHT
+
     mapWidth = 20
     mapHeight = 20
 
     -- amount by which we'll translate the scene to emulate a camera
     cameraScroll = 0
 
-    backgroundR = math.random(255) / 255 / 255
-    backgroundG = math.random(255) / 255 / 255
-    backgroundB = math.random(255) / 255 / 255
+    backgroundR = math.random(255) / 255
+    backgroundG = math.random(255) / 255
+    backgroundB = math.random(255) / 255
 
     for y = 1, mapHeight do
         table.insert(tiles, {})
@@ -52,7 +65,7 @@ function love.load()
         for x = 1, mapWidth do
             -- sky and bricks; this ID directly maps to whatever quad we want to render
             table.insert(tiles[y], {
-                id = y < 5 and SKY or GROUND
+                id = y < 7 and SKY or GROUND
             })
         end
     end
@@ -80,9 +93,9 @@ end
 function love.update(dt)
     -- update camera scroll based on user input
     if love.keyboard.isDown('left') then
-        cameraScroll = cameraScroll - CAMERA_SCROLL_SPEED * dt
+        characterX = characterX - CHARACTER_MOVE_SPEED * dt
     elseif love.keyboard.isDown('right') then
-        cameraScroll = cameraScroll + CAMERA_SCROLL_SPEED * dt
+        characterX = characterX + CHARACTER_MOVE_SPEED * dt
     end
 end
 
@@ -101,5 +114,8 @@ function love.draw()
                 love.graphics.draw(tilesheet, quads[tile.id], (x - 1) * TILE_SIZE, (y - 1) * TILE_SIZE)
             end
         end
+
+        -- draw character
+        love.graphics.draw(characterSheet, characterQuads[1], math.floor(characterX), math.floor(characterY))
     push:finish()
 end
